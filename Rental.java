dae305f1e1283110cd1c4731e7532c2cd3bf16cd
Anne Isabelle Rodrigues de Carvalho <anneisabelle.rodrigues@outlook.com>
2022-02-05 20:31:07 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -2,24 +2,35 @@ public class Rental {
 
    private Movie _movie;
    private int _daysRented;
+   private int frequentRenterPoints = 0;
+
+   public int getFrequentRenterPoints() {
+    // add bonus for a two day new release rental
+    if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) frequentRenterPoints ++;
+
+    frequentRenterPoints ++;
+
+    return frequentRenterPoints;
+
+   }
 
    public double getCharge(){
     //determine amounts for each line
-        double thisAmount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-              thisAmount += 2;
-              if (this.getDaysRented() > 2)
-                  thisAmount += (this.getDaysRented() - 2) * 1.5;
-              break;
-            case Movie.NEW_RELEASE:
-              thisAmount += this.getDaysRented() * 3;
-              break;
-            case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (this.getDaysRented() > 3)
-                  thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
+    double thisAmount = 0;
+    switch (this.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          thisAmount += 2;
+          if (this.getDaysRented() > 2)
+              thisAmount += (this.getDaysRented() - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          thisAmount += this.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (this.getDaysRented() > 3)
+              thisAmount += (this.getDaysRented() - 3) * 1.5;
+            break;
     }
 
     return thisAmount;
