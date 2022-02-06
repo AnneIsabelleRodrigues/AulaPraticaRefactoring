e1d362987b48fc901c64e87f1672accb5cffce01
Anne Isabelle Rodrigues de Carvalho <anneisabelle.rodrigues@outlook.com>
2022-02-05 21:11:08 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -3,4 +3,14 @@ public abstract class Price {
 
    public abstract double getCharge(int daysRented);
 
+  public int getFrequentRenterPoints(int daysRented) {
+    int frequentRenterPoints = 0;
+
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) frequentRenterPoints ++;
+
+    frequentRenterPoints ++;
+
+    return frequentRenterPoints;
+  }
+
 }
